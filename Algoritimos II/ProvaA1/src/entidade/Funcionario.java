f5d15fac5422c37c6766645de1cbc777fa3a1970
Rafael Brito <rafaelbrito.web@gmail.com>
2018-10-02 17:39:28 -0300

    ProvaA1
    Começando a desenvolver a prova, para estudo

---
+++ Algoritimos II/ProvaA1/src/entidade/Funcionario.java
@@ -0,0 +1,9 @@
+package entidade;
+
+public class Funcionario {
+
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private int telefone;
+}
---
+++ Algoritimos II/ProvaA1/src/entidade/Moto.java
@@ -0,0 +1,5 @@
+package entidade;
+
+public class Moto extends Veiculo{
+
+}
